6d97545fc746891646efc2adb73645a08739e2a3
mae14828 <migi2412@outlook.jp>
2026-09-05 19:44:46 +0900

    practice11を追加し、構造体の基本的な部分だけ書いた。

---
+++ practice11/practice11.c
@@ -0,0 +1,34 @@
+//図形の体積計算プログラム
+#include<stdio.h>
+
+typedef struct {
+	char syurui;
+	double height;
+	double width;
+	double depth;
+} Shape;
+
+//図形の体積を計算
+double calculateVolume(Shape s) {
+	switch (s) {
+	
+	default:
+		return 0.0;
+	}
+}
+
+int main() {
+	Shape shape[5] = {
+		{'T',10,4,5},
+		{'P',9,3,2},
+		{'R',6,4,3},
+		{'P',2,2,2},
+		{'T',8,3,3}
+	};
+
+	printf("データ一覧:\n");
+	for (int i = 0; i < 5; i++) {
+		printf("種類: %c,H: %.1lf, W: %.1lf, D:%.1lf\n", shape[i].syurui, shape[i].height, shape[i].width, shape[i].depth);
+	}
+
+}
