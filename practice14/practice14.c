7d54eacf620f5f320ff8bbb2c3a8094aea8d8350
mae14828 <migi2412@outlook.jp>
2026-09-15 23:54:44 +0900

    practice14で、ソートをした。ポインタの練習にはなっていない気がする。

--- practice14/practice14.c
+++ practice14/practice14.c
@@ -1,8 +1,9 @@
 //ポインタ練習問題
 //最大値と最小値を求める
 #include<stdio.h>
-#define NUM 5
+#define NUM 10
 
+int max(int array[]);
 
 int main() {
 	int data[NUM];
@@ -10,5 +11,21 @@ int main() {
 	for (int i = 0; i < NUM; i++) {
 		scanf("%d", &data[i]);
 	}
+	max(data);
+
+	return 0;
+}
+
+int max(int array[]) {
+	for (int i = 0; i < NUM-1; i++) {
+		for (int j = 0 ; j < NUM-i-1; j++) {
+			if (array[j] < array[j + 1]) {
+				int tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+			}
+		}
+	}
+	printf("最大値は%d\n",array[0]);
 	return 0;
 }
