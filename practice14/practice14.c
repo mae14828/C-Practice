d730ddd9c0e3186a0bce671169383c203dbc894f
mae14828 <migi2412@outlook.jp>
2026-09-17 18:12:41 +0900

    practice14を完成させた。ポインタを用いたmaxMinCulc関数によって最大値と最小値を求める。

--- practice14/practice14.c
+++ practice14/practice14.c
@@ -3,26 +3,30 @@
 #include<stdio.h>
 #define NUM 10
 
-void maxCulc(int array[], int* max);
+void maxMinCulc(int array[], int* max, int* min);
 
 int main() {
 	int data[NUM];
-	int max;
+	int max, min;
 	printf("%d個の数字を入力：\n",NUM);
 	for (int i = 0; i < NUM; i++) {
 		scanf("%d", &data[i]);
 	}
-	maxCulc(data, &max);
-	printf("最大値は%d\n", max);
+	maxMinCulc(data, &max, &min);
+	printf("最大値は%d\n最小値は%d\n", max, min);
 
 	return 0;
 }
 
-void maxCulc(int array[], int* max) {
+void maxMinCulc(int array[], int* max, int* min) {
 	*max = *array; //*arrayはarray[0]と同義
+	*min = *array;
 	for (int i = 1; i < NUM ; i++) {
 		if (*max < *(array + i)) {
 			*max = *(array + i); //*(array+i)はarray[i]と同義
+		}
+		if (*min > *(array + i)) {
+			*min = *(array + i);
 		}
 	}
 }
